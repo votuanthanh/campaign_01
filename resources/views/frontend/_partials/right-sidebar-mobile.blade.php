aa2a097620b3bc2fdb499799288fdd616fe1f57b
Nguyen Van Ngoc <ngocnguyencnttk35@gmail.com>
2017-05-16 16:10:34 +0700

    frontend master layout

---
+++ resources/views/frontend/_partials/right-sidebar-mobile.blade.php
@@ -0,0 +1,9 @@
+<div class="fixed-sidebar right fixed-sidebar-responsive">
+    <div class="fixed-sidebar-right sidebar--small" id="sidebar-right-responsive">
+        <a href="#" class="olympus-chat inline-items js-chat-open">
+            <svg class="olymp-chat---messages-icon">
+                <use xlink:href="frontend/icons/icons.svg#olymp-chat---messages-icon"></use>
+            </svg>
+        </a>
+    </div>
+</div>
