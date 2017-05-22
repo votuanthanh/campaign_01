ad286a8bcc401436037c1a5c6fd310a3a34dff7b
Ho Thi Thien Trang <ho.thi.thien.trang@framgia.com>
2017-05-22 13:00:40 +0700

    API Authentication (Passport)

--- config/app.php
+++ config/app.php
@@ -169,6 +169,7 @@ return [
          * Package Service Providers...
          */
         Laravel\Tinker\TinkerServiceProvider::class,
+        Laravel\Passport\PassportServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -232,7 +233,7 @@ return [
         'Form' => Collective\Html\FormFacade::class,
         'Html' => Collective\Html\HtmlFacade::class,
         'Debugbar' => Barryvdh\Debugbar\Facade::class,
-        
+
     ],
 
 ];
