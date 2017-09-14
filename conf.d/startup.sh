d1370620e2638647acca424b898378bfa74c0bcc
huynhdiep <it.huynhdiep@gmail.com>
2017-09-14 13:24:28 +0700

    suppervisor

--- conf.d/startup.sh
+++ conf.d/startup.sh
@@ -4,3 +4,5 @@
 systemctl restart apache2
 pm2 start Node/server.js
 /usr/sbin/apache2ctl -D FOREGROUND
+supervisord -c /etc/supervisor/supervisord.conf
+supervisorctl -c /etc/supervisor/supervisord.conf
